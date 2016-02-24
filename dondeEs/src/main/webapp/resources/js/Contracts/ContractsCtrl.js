fcada8b609cc001bb34a6ebbdb273c47091bcec2
Juan Carlos Sanchez <jcarlos0305@hotmail.com>
2016-02-24 13:02:11 -0600

    Listar contratos

--- dondeEs/src/main/webapp/resources/js/Contracts/ContractsCtrl.js
+++ dondeEs/src/main/webapp/resources/js/Contracts/ContractsCtrl.js
@@ -8,6 +8,16 @@ angular
 			});
 		} ])
 		.controller('ContractsCtrl',['$scope','$http',function($scope, $http) {
+			
+			$scope.listContracts = function() {
+				$http.post("rest/login/checkuser/",$scope.user).success(function(response){
+					if(response.code == 200){
+						
+					}else{
+						
+					}
+				});
+			}
 		
 			
 			
