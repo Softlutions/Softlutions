c889ab0702469d4f1e190e465f9768c601203e1e
antonirm <tonyxrm@gmail.com>
2016-03-05 16:02:10 -0600

    Adding config file and comment in asnwer invitation

--- dondeEs/src/main/java/com/cenfotec/dondeEs/controller/SendEmailController.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/controller/SendEmailController.java
@@ -7,10 +7,12 @@ import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.ws.rs.QueryParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,60 +23,37 @@ import com.cenfotec.dondeEs.pojo.ListSimplePOJO;
 @RequestMapping(value = "rest/protected/sendEmail")
 public class SendEmailController {
 
-	private static final String from = "evaluacionescenfotec@gmail.com";
-	private static final String host = "smtp.gmail.com";
-	private static final String pass = "evaluacionescenfotec2015";
 	private static String subject;
 	private static String text;
-	private static Properties props;
 	
-	public static void generalEmail(){
-		 props = System.getProperties();
-
-		props.put("mail.smtp.starttls.enable", "true");
-
-		props.put("mail.smtp.ssl.trust", host);
-		props.put("mail.smtp.user", from);
-		props.put("mail.smtp.password", pass);
-		props.put("mail.smtp.port", "587");
-		props.put("mail.smtp.auth", "true");
-
-	}
-	
+	   @Autowired
+	   private JavaMailSender mailSender;
+	     
 	@RequestMapping(value = "/sendEmailInvitation", method = RequestMethod.POST)
 	public void sendEmailInvitation(@RequestBody ListSimplePOJO to, @QueryParam("eventId") int eventId) {
-				
-		generalEmail();
-		Session session = Session.getDefaultInstance(props);
+		
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+
 		subject = "Invitacion a un evento";
 		try {
 
-			
-			Transport transport = session.getTransport("smtp");
-
 			// To get the array of addresses
 			for (String email : to.getListSimple()) {
-				MimeMessage message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(from));
 				
 				text = "http://localhost:8080/dondeEs/app#/answerInvitation?eventId="+ eventId+"&email="+email;
+			
+				mailMessage.setTo(email);
+				mailMessage.setText(text);
+				mailMessage.setSubject(subject);
+				mailSender.send(mailMessage);
 				
-				InternetAddress internetAddress = new InternetAddress(email);
-				message.addRecipient(Message.RecipientType.TO, internetAddress);
-				message.setSubject(subject);
-				message.setText(text);
-				transport.connect(host, from, pass);
-				transport.sendMessage(message, message.getAllRecipients());
-
 			}
-			transport.close();
 
 			
-		} catch (AddressException ae) {
+		} catch (Exception ae) {
 			ae.printStackTrace();
-		} catch (MessagingException me) {
-			me.printStackTrace();
-		}
+		} 
 	}
 	
 
--- dondeEs/src/main/java/com/cenfotec/dondeEs/services/CommentService.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/services/CommentService.java
@@ -3,9 +3,18 @@ package com.cenfotec.dondeEs.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cenfotec.dondeEs.ejb.Comment;
 import com.cenfotec.dondeEs.repositories.CommentRepository;
 
 @Service
 public class CommentService implements CommentServiceInterface {
 	@Autowired private CommentRepository commentRepository;
+
+	@Override
+	public Boolean saveComment(Comment comment) {
+		Comment ncomment = commentRepository.save(comment);
+		return (ncomment == null)? false : true;
+	}
+	
+	
 }
