f48f7ed7caaa227c143a5c5888817bc03519837b
Juan Carlos Sanchez <jcarlos0305@hotmail.com>
2016-03-07 10:29:13 -0600

    hotfix

--- dondeEs/src/main/java/com/cenfotec/dondeEs/services/ChatService.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/services/ChatService.java
@@ -1,4 +1,4 @@
- package com.cenfotec.dondeEs.services;
+package com.cenfotec.dondeEs.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
