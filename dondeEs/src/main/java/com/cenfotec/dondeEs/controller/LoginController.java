11f166c7dae12847c33cb349c298cd237e5fa764
AlejandroBermudezVargas <abermudezv@ucenfotec.ac.cr>
2016-03-13 13:47:34 -0600

    Add method to send email with new password

--- dondeEs/src/main/java/com/cenfotec/dondeEs/controller/LoginController.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/controller/LoginController.java
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cenfotec.dondeEs.contracts.BaseResponse;
 import com.cenfotec.dondeEs.contracts.LoginRequest;
 import com.cenfotec.dondeEs.contracts.LoginResponse;
+import com.cenfotec.dondeEs.contracts.UserRequest;
+import com.cenfotec.dondeEs.contracts.UserResponse;
 import com.cenfotec.dondeEs.services.LoginServiceInterface;
+import com.cenfotec.dondeEs.services.UserServiceInterface;
 
 
 /**
@@ -25,7 +28,7 @@ import com.cenfotec.dondeEs.services.LoginServiceInterface;
 public class LoginController {
 	
 	@Autowired private LoginServiceInterface loginService;
-	
+	@Autowired private UserServiceInterface userServiceInterface;
 	/**
 	 * @Author Ernesto Méndez A.
 	 * @param lr Petición que contiene el email y la contraseña del usuarioa logear
@@ -43,5 +46,18 @@ public class LoginController {
 		
 		return response;
 		
+	}
+	
+	@RequestMapping(value ="/updatePassword", method = RequestMethod.POST)
+	public UserResponse updatePassword(@RequestBody LoginRequest lr,HttpServletRequest servletRequest,HttpServletResponse servletResponse){	
+		UserResponse us = new UserResponse();
+		if(userServiceInterface.updatePassword(lr)){
+			us.setCode(200);
+			us.setCodeMessage("Password cambiado exitosamente");
+		}else{
+			us.setCode(401);
+			us.setCodeMessage("No se pudo cambiar la contraseña");
+		}
+		return us;
 	}
 }
