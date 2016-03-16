8c1877fd04741d43ba4b7b0d67d28f2bec4ed483
antonirm <tonyxrm@gmail.com>
2016-03-16 15:50:50 -0600

    Working front end chat need style. Logic ready

--- dondeEs/src/main/java/com/cenfotec/dondeEs/services/MessageService.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/services/MessageService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cenfotec.dondeEs.ejb.Message;
+import com.cenfotec.dondeEs.pojo.ChatPOJO;
 import com.cenfotec.dondeEs.pojo.MessagePOJO;
+import com.cenfotec.dondeEs.pojo.UserPOJO;
 import com.cenfotec.dondeEs.repositories.MessageRepository;
 
 import org.springframework.beans.BeanUtils;
@@ -29,7 +31,30 @@ public class MessageService implements MessageServiceInterface {
 		listMessage.stream().forEach(m ->{
 			MessagePOJO message = new MessagePOJO();
 			BeanUtils.copyProperties(m, message);
-			
+			if(m.getUser() != null){
+				UserPOJO userPOJO = new UserPOJO();
+				
+				BeanUtils.copyProperties(m.getUser(), userPOJO);
+				userPOJO.setEventParticipants(null);
+				userPOJO.setMessages(null);
+				userPOJO.setPasswordHistories(null);
+				userPOJO.setTermConditions(null);
+				userPOJO.setChats(null);
+				userPOJO.setUserType(null);
+				userPOJO.setUsers1(null);
+				userPOJO.setUsers2(null);
+				message.setUser(userPOJO);
+			}
+			if(m.getChat() != null){
+				ChatPOJO chatPOJO = new ChatPOJO();
+				
+				BeanUtils.copyProperties(m.getChat(), chatPOJO);
+				chatPOJO.setEvent(null);
+				chatPOJO.setMessages(null);
+				chatPOJO.setUsers(null);
+				
+				message.setChat(chatPOJO);
+			}
 			messagePOJOs.add(message);
 		});
 		return messagePOJOs;
--- dondeEs/src/main/webapp/resources/js/Chat/ChatController.js
+++ dondeEs/src/main/webapp/resources/js/Chat/ChatController.js
@@ -13,21 +13,66 @@ angular
 						'$scope',
 						'$http','$location',
 						function($scope, $http, $location) {
+							$("#messageByChat").hide();
+							$scope.objMessage ={};
 							$scope.loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
 							
-								$http({method: 'GET',url:'rest/protected/getChatsByUser/'+$$scope.loggedUser.userId, headers: {'Content-Type': 'application/json'}}).success(function(response) {
+//							$(document).ready(function() {
+//							
+//									    // show when page load
+//									    toastr.info('Page Loaded!');
+//							
+//									    $('#linkButton').click(function() {
+//									    	 toastr.options = {
+//									                    closeButton: true,
+//									                    progressBar: true,
+//									                    showMethod: 'slideDown',
+//									                    timeOut: 4000
+//									                };
+//									                toastr.success('Responsive Admin Theme', 'Welcome to INSPINIA');
+//							
+//									    });
+//							
+//									});
+							
+								$http({method: 'GET',url:'rest/protected/chat/getChatsByUser/'+$scope.loggedUser.userId, headers: {'Content-Type': 'application/json'}}).success(function(response) {
 									$scope.chats = response.chats;
-							}
-								
-							$scope.sendMessage = function(pcontent){
-								
-								var dataCreate ={
-										user : loggedUser,
-										chat : chat,
-										content = pcontent
+								})
+									
+							$scope.getAllMessage = function(idChat){
+									$("#messageByChat").show();
+									$scope.chat = {chatId:idChat}
+									$http({method: 'GET',url:'rest/protected/message/getAllMessageByChat/'+idChat, headers: {'Content-Type': 'application/json'}}).success(function(response) {
+										
+										$scope.messages = response.messages;
+										
+										
+									})
 								}
 								
-								$http({method: 'POST',url:'rest/protected/message/createMessage', data:dataCreate, headers: {'Content-Type': 'application/json'}}).success(function(response) {
-								})
+							$scope.sendMessage = function(event){
+								
+								var dataCreate ={
+										user : $scope.loggedUser,
+										chat : $scope.chat,
+										content : $scope.objMessage.content
+										
+								}
+								if($scope.objMessage.content != null){
+									$http({method: 'POST',url:'rest/protected/message/createMessage', data:dataCreate, headers: {'Content-Type': 'application/json'}}).success(function(response) {
+										$scope.messages = $scope.messages.concat(dataCreate);
+									})
+								}else{
+									 setTimeout(function() {					
+							                toastr.options = {
+							                    closeButton: true,
+							                    progressBar: true,
+							                    showMethod: 'slideDown',
+							                    timeOut: 4000
+							                };
+							                toastr.error('No puede enviar mensajes en blanco', 'Error');
+
+							            }, 1300);
+								}
 							}
 				}])
