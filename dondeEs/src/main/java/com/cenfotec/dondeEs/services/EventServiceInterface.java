2eae7512ee3e0b284d845e352ba1b02cccccfc31
egarcia96 <egarciag@ucenfotec.ac.cr>
2016-03-19 19:28:23 -0600

    Eliminar console.log(); Agregar carpeta “general-image” en la carpeta Resource del webapp para el almacenamiento de las imágenes. En los mensajes de notificación el toast options solo se pone una vez. Alineación de elementos en html de crear evento. En el asistente (crear evento), no dejar subir un archivo que no sea .jpg,  .png o gif

--- dondeEs/src/main/java/com/cenfotec/dondeEs/services/EventServiceInterface.java
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/services/EventServiceInterface.java
@@ -8,6 +8,13 @@ import com.cenfotec.dondeEs.pojo.EventPOJO;
 public interface EventServiceInterface {
 	List<EventPOJO> getAllEventByUser(int pidUsuario);
 	Event getEventById(int idEvent);
+	
+	/***
+	 * Guarda un evento.
+	 * @author Enmanuel García González
+	 * @return True en caso de efectuarse la inserción o false en caso contrario.
+	 * @version 1.0
+	 */
 	int saveEvent(Event e);
 	EventPOJO eventById(int idEvent);
 	List<EventPOJO> getAllEventPublish();
