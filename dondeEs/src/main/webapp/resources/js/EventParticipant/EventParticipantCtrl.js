649c978c70d88cce17c7c583c5ed24f7375a5a11
DeusAnimaX <emendeza@ucenfotec.ac.cr>
2016-04-07 16:23:49 -0600

    filter in users list chat multitheading problem solved improve landingPage menu issues solved

--- dondeEs/src/main/webapp/resources/js/EventParticipant/EventParticipantCtrl.js
+++ dondeEs/src/main/webapp/resources/js/EventParticipant/EventParticipantCtrl.js
@@ -7,14 +7,13 @@ angular
 			controller : 'EventParticipantCtrl'
 		});
 	} ])
-	.controller('EventParticipantCtrl',['$scope','$http','$routeParams','ngTableParams',function($scope, $http, $routeParams,ngTableParams) {
+	.controller('EventParticipantCtrl',['$scope','$http','$routeParams','ngTableParams','$interval',function($scope,$http,$routeParams,ngTableParams,$interval) {
 		
 		$http.get('rest/protected/eventParticipant/getAllEventParticipants/'+$routeParams.id).success(function(response) {
 			$scope.participants = response.eventParticipantsList;
 			var params = {
-				page: 1,	// PAGINA INICIAL
-				count: 10 	// CANTIDAD DE ITEMS POR PAGINA
-				//sorting: {name: "asc"}
+				page: 1,
+				count: 20
 			};
 				
 			var settings = {
@@ -30,5 +29,17 @@ angular
 			$scope.participantsTable = new ngTableParams(params, settings);
 		});
 		
+		$scope.refreshInterval = $interval(function() {
+			if($routeParams.id != null){
+				$http.get('rest/protected/eventParticipant/getAllEventParticipants/'+$routeParams.id).success(function(response) {
+					$scope.participants = response.eventParticipantsList;
+					$scope.participantsTable.reload();
+				});
+			}
+        }, 3000);
+		
+		$scope.$on('$destroy', function() {
+			$interval.cancel($scope.refreshInterval);
+		});
 		
 	}]);
