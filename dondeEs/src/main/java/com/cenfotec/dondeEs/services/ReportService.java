f75c056f39cf13299b392797a350a83905651e70
pigleth96 <egarciag@ucenfotec.ac.cr>
2016-04-10 17:39:47 -0600

    add reports system files (back end)

---
+++ dondeEs/src/main/java/com/cenfotec/dondeEs/services/ReportService.java
@@ -0,0 +1,18 @@
+package com.cenfotec.dondeEs.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cenfotec.dondeEs.pojo.ParticipationOnEventsPOJO;
+
+@Service
+public class ReportService implements ReportServiceInterface{
+
+	@Override
+	public List<ParticipationOnEventsPOJO> getParticipationOnEvents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
