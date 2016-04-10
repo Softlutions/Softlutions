fc15e21bb0c5aeee87dcbb5b1690cc314369f9ff
Juan Carlos Sanchez <jcarlos0305@hotmail.com>
2016-04-10 09:43:55 -0600

    Ver perfil de usuario, modificar perfil de usuario, eliminar notas, fix notas

--- dondeEs/src/main/webapp/resources/js/users/UserProfileController.js
+++ dondeEs/src/main/webapp/resources/js/users/UserProfileController.js
@@ -6,12 +6,34 @@ angular.module('dondeEs.userProfile', ['ngRoute']).config(['$routeProvider', fun
 			controller : 'UserProfileCtrl'
 		});
 	}]).controller('UserProfileCtrl', ['$scope', '$http','$routeParams',  function($scope, $http,$routeParams) {
-		
+		$scope.$parent.pageTitle = "Donde es - Perfil de usuario";
+		$scope.loggedUser = JSON.parse(localStorage.getItem("loggedUser"));
 		
 		$http.get("rest/protected/users/getUserById/"+$routeParams.id).success(function(response){
 			$scope.user = response.user;
 			console.log($scope.user);
 		});
+		
+		$scope.validationError = function(){
+			toastr.warning('Algunos campos no cumplen con los requisitos');
+		}
+		
+		$scope.updateUser= function(){
+			var dataUpdate ={
+				userId : $scope.user.userId,
+				name: $scope.user.name,
+				lastName1 : $scope.user.lastName1,
+				lastName2 :$scope.user.lastName2,
+				email: $scope.user.email,
+				phone : $scope.user.phone,
+				userType : $scope.user.userType
+			}
+			$http.put('rest/protected/users/updateUser',dataUpdate).success(function(response) {
+				$("#modal-formUpdate").modal("toggle");
+				toastr.success("Se ha modificado la información del usuario.")
+				
+			});
+		}
 		
 		
 	}]);
